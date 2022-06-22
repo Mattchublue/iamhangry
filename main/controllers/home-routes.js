3315fc94ab8ab07c607b9602c3a1e81d919abfa6
Matthew <mwhiteman83@gmail.com>
2022-06-21 19:33:11 -0600

    added handlebars

--- main/controllers/home-routes.js
+++ main/controllers/home-routes.js
@@ -91,7 +91,7 @@ router.get('/post/:id', (req, res) => {
 
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect('/');
+    res.redirect('/login');
     return;
   }
 
